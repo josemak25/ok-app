92d87d694e11ebc5cab976c7278cf19af2cbb14c
josemak25 <amakirij@gmail.com>
2020-06-11 11:15:05 +0100

    add react native screens for navigation optimzation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
+import { enableScreens } from 'react-native-screens';
 import AppLoading from './src/components/AppLoading';
 import AppRouter from './src';
 
 export default function App() {
+  enableScreens();
+
   const [isAppReady, setIsAppReady] = useState(false);
 
   return isAppReady ? (
