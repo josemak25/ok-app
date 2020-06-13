a4a0e71d5cdc8ff63160f8192da46f3429cf27fa
josemak25 <amakirij@gmail.com>
2020-06-13 09:44:28 +0100

    add home screen component styles

--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,8 +1,21 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.DARK_BG_COLOR};
+  padding: 0px 20px;
+`;
+
+export const WelcomeText = styled.Text`
+  font-size: ${({ theme }) => RFValue(theme.fonts.LARGE_SIZE * 2)}px;
+  font-family: ${({ theme }) => theme.fonts.CORE_SANS_BOLD};
+  color: ${({ theme }) => theme.colors.BLACK_FONT_COLOR};
+  padding-top: 10px;
+`;
+
+export const SearchJob = styled.TextInput`
+  font-size: ${({ theme }) => RFValue(theme.fonts.LARGE_SIZE)}px;
+  font-family: ${({ theme }) => theme.fonts.CORE_SANS_BOLD};
+  color: ${({ theme }) => theme.colors.BLACK_FONT_COLOR};
+  margin-top: 20px;
 `;
