bd5aa695d2f7eb40c0271b89c0b67a2178a72c8e
josemak25 <amakirij@gmail.com>
2020-06-13 09:39:34 +0100

    add card component styles

---
+++ src/components/card/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.TouchableOpacity`
+  align-items: center;
+  background-color: ${({ theme }) => theme.colors.WHITE_BG_COLOR};
+`;
