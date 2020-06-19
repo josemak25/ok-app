0713b926324e0b5925e26ea942ade03bf61ce187
josemak25 <amakirij@gmail.com>
2020-06-19 17:36:24 +0100

    add safe view area for details page for displaying a job details

--- src/screens/detail/index.tsx
+++ src/screens/detail/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
-import { Container } from './styles';
 import { WebView } from 'react-native-webview';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { NavigationInterface } from '../types';
 import { useTheme } from '../../theme';
+import jsCode from './jsCode';
+import LoadingJobs from '../../components/loadingJobs';
+
+import { Container } from './styles';
 
 interface DetailScreenProps extends NavigationInterface {
   testID?: string;
@@ -10,52 +14,30 @@ interface DetailScreenProps extends NavigationInterface {
 
 export default function DetailScreen(props: DetailScreenProps) {
   const { colors } = useTheme();
-  const { navigation, route } = props;
+  const { route } = props;
 
   //@ts-ignore
   const { jobLink } = route.params;
 
-  const jsCode = `
-  document.querySelector('.page').style.backgroundColor = '#F8FBFB';
-
-  document.querySelector('.notice').style.display = 'none';
-  document.querySelector('.nav').style.display = 'none';
-  document.querySelector('.switcher-container').style.display = 'none';
-  document.querySelector('.notice-post-job').style.display = 'none';
-  document.querySelector('.feedback-popup').style.display = 'none';
-  document.querySelector('.open-startup').style.display = 'none';
-  const tableBody = document.querySelector('tbody').children
-  tableBody[0].style.display = 'none';
-  tableBody[1].style.display = 'none';
-
-  const instructionsBody = document.querySelector('.instructions').children
-  instructionsBody[0].style.display = 'none';
-
-  const expandContents =  document.querySelector('.expandContents')
-  const expandContentsChildren = expandContents.children
-
-  expandContents.style.display = 'none';
-
-  const newExpandContents = document.createElement("div");
-  newExpandContents.style.display = 'block';
-  newExpandContents.style.width = '100%';
-  newExpandContents.style.height = '300px';
-  newExpandContents.style.backgroundColor = '#ff4742';
-
-
-  // newExpandContents.appendChild(expandContentsChildren);
-  tableBody.appendChild(newExpandContents);
-  `;
-
   return (
-    <Container>
+    <SafeAreaView
+      style={{ flex: 1, backgroundColor: colors.DARK_BG_COLOR, paddingTop: 0 }}
+    >
       <WebView
         source={{ uri: jobLink }}
-        containerStyle={{ flex: 1, backgroundColor: colors.DARK_BG_COLOR }}
-        originWhitelist={['*']}
         javaScriptEnabled={true}
         injectedJavaScript={jsCode}
+        showsVerticalScrollIndicator={false}
+        startInLoadingState={true}
+        scalesPageToFit={true}
+        renderLoading={() => (
+          <Container>
+            <LoadingJobs />
+          </Container>
+        )}
+        style={{ flex: 1, backgroundColor: colors.DARK_BG_COLOR }}
+        containerStyle={{ flex: 1, backgroundColor: colors.DARK_BG_COLOR }}
       />
-    </Container>
+    </SafeAreaView>
   );
 }
