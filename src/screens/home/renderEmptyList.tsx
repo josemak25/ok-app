c1cd3297e5204ff71490542d4eef4e6fc49240f9
josemak25 <amakirij@gmail.com>
2020-06-21 07:21:48 +0100

    add render empty list conditions properly for rendering when fetching data

--- src/screens/home/renderEmptyList.tsx
+++ src/screens/home/renderEmptyList.tsx
@@ -1,65 +1,74 @@
 import React from 'react';
+import { ScrollView } from 'react-native';
 import SkeletonContent from 'react-native-skeleton-content';
 import { useTheme } from '../../theme';
 import { RFValue } from 'react-native-responsive-fontsize';
 
-export default function _renderEmptyList() {
+type RenderEmptyListProps = { isLoading: boolean; renderHeader(): void };
+
+export default function RenderEmptyList(props: RenderEmptyListProps) {
   const { colors } = useTheme();
 
-  return [...new Array(5)].map((_, index) => (
-    <SkeletonContent
-      key={index.toString()}
-      boneColor={colors.DARK_BG_COLOR}
-      containerStyle={{
-        height: 200,
-        width: '100%',
-        backgroundColor: colors.WHITE_BG_COLOR,
-        padding: 20,
-        marginTop: 4,
-        marginBottom: 4
-      }}
-      isLoading={true}
-      layout={[
-        { width: RFValue(120), height: 20, marginBottom: 10 },
+  const { isLoading, renderHeader } = props;
 
-        { width: RFValue(200), height: 30, marginBottom: 6 },
+  return (
+    <ScrollView style={{ flex: 1 }} showsVerticalScrollIndicator={false}>
+      {renderHeader()}
+      {[...new Array(5)].map((_, index) => (
+        <SkeletonContent
+          key={index.toString()}
+          isLoading={isLoading}
+          containerStyle={{
+            height: 200,
+            width: '100%',
+            backgroundColor: colors.WHITE_BG_COLOR,
+            padding: 20,
+            marginTop: 4,
+            marginBottom: 4
+          }}
+          layout={[
+            { width: RFValue(120), height: 20, marginBottom: 10 },
 
-        { width: RFValue(70), height: RFValue(25), marginTop: 20 },
+            { width: RFValue(200), height: 30, marginBottom: 6 },
 
-        {
-          width: RFValue(90),
-          height: RFValue(25),
-          marginTop: 30,
-          position: 'absolute',
-          left: RFValue(100),
-          top: 75
-        },
+            { width: RFValue(70), height: RFValue(25), marginTop: 20 },
 
-        { width: RFValue(100), height: RFValue(25), marginTop: 10 },
+            {
+              width: RFValue(90),
+              height: RFValue(25),
+              marginTop: 30,
+              position: 'absolute',
+              left: RFValue(100),
+              top: 75
+            },
 
-        {
-          width: RFValue(30),
-          height: RFValue(20),
-          position: 'absolute',
-          right: 35,
-          top: 20
-        },
-        {
-          width: RFValue(60),
-          height: RFValue(60),
-          position: 'absolute',
-          right: 20,
-          top: 70,
-          borderRadius: RFValue(60 / 2)
-        },
-        {
-          width: RFValue(60),
-          height: RFValue(20),
-          position: 'absolute',
-          right: 20,
-          bottom: 20
-        }
-      ]}
-    ></SkeletonContent>
-  ));
+            { width: RFValue(100), height: RFValue(25), marginTop: 10 },
+
+            {
+              width: RFValue(30),
+              height: RFValue(20),
+              position: 'absolute',
+              right: 35,
+              top: 20
+            },
+            {
+              width: RFValue(60),
+              height: RFValue(60),
+              position: 'absolute',
+              right: 20,
+              top: 70,
+              borderRadius: RFValue(60 / 2)
+            },
+            {
+              width: RFValue(60),
+              height: RFValue(20),
+              position: 'absolute',
+              right: 20,
+              bottom: 20
+            }
+          ]}
+        ></SkeletonContent>
+      ))}
+    </ScrollView>
+  );
 }
