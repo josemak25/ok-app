3a20350ca93e526d31886dc0b719d37b58aa95ca
josemak25 <amakirij@gmail.com>
2020-07-01 17:23:54 +0100

    connect reactotron on app config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,6 @@
 //@ts-nocheck
 import { DATABASE_URI } from 'react-native-dotenv';
+import Reactotron from 'reactotron-react-native';
 
 export type ENV_VARIABLES_TYPES = { DATABASE_URI: string };
 
@@ -7,3 +8,16 @@ export type ENV_VARIABLES_TYPES = { DATABASE_URI: string };
 const ENVIRONMENT_VARIABLES = { DATABASE_URI } as ENV_VARIABLES_TYPES;
 
 export default ENVIRONMENT_VARIABLES;
+
+if (__DEV__) {
+  Reactotron.configure({ name: 'OkApp' }).useReactNative().connect();
+
+  // Extend console with tron for being able to debug to Reactotron
+  console.tron = Reactotron.log;
+}
+
+declare global {
+  interface Console {
+    tron(param?: any, ...optionalParams: any[]): void;
+  }
+}
