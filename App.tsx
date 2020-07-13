5c56a8711d6e66a2fb2be4d595379afacbad6031
josemak25 <amakirij@gmail.com>
2020-07-13 22:42:41 +0100

    remove enable screen from app start to fix app naviation to detail page crashes

--- App.tsx
+++ App.tsx
@@ -1,12 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import * as SplashScreen from 'expo-splash-screen';
-import { enableScreens } from 'react-native-screens';
 import loadResources from './src/libs/loadResources';
 import AppRouter from './src';
 
 export default function App() {
-  enableScreens();
-
   const [isAppReady, setIsAppReady] = useState(false);
 
   useEffect(() => {
