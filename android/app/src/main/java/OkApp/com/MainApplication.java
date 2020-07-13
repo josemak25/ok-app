03cd0f39cd4ad524a172c485e2fb2488105164a3
josemak25 <amakirij@gmail.com>
2020-07-14 00:53:03 +0100

    add fix for build crash

--- android/app/src/main/java/OkApp/com/MainApplication.java
+++ android/app/src/main/java/OkApp/com/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.net.Uri;
 
+import com.OkApp.BuildConfig;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.dylanvann.fastimage.FastImageViewPackage;
